4939d52834c764bf4ee634387467991b8fd6ced7
Omar Younis <omarn.younis@gmail.com>
2021-10-11 00:47:40 +0200

    Add top function

--- functions.cpp
+++ functions.cpp
@@ -1,193 +1,5 @@
 #include "includes.h"
 
-//#pragma HLS unroll
-
-ap_uint<SIZE_N> barrett_reduction(
-	ap_uint<SIZE_N> modulus
-	){
-
-	ap_uint<SIZE_N> temp_1;
-	ap_uint<SIZE_N+1> temp_2;
-	ap_uint<2*SIZE_N+1> temp_3;
-
-	ap_uint<SIZE_N> result;
-
-	temp_1 = ap_uint<SIZE_N+1>(R_CONST) >> (SIZE_N - 1);
-	temp_2 = ap_uint<2*SIZE_N+1>(R_CONST2) / modulus;
-
-	temp_3 = temp_1 * temp_2;
-
-	result = temp_3 >> (SIZE_N - 1);
-	return result;
-}
-
-ap_uint<SIZE_N> square_r(
-	ap_uint<SIZE_N> modulus
-	){
-
-	ap_uint<SIZE_N> r_reduced;
-
-	r_reduced = barrett_reduction(modulus);
-
-	return barrett_reduction(r_reduced * r_reduced);
-}
-
-#if(MONT_IMP == 1)
-    ap_uint<SIZE_N> rad2_mont_mult(
-    	/*Source: A Scalable Architecture for Montgomery Multiplication*/
-        ap_uint<SIZE_N> multiplicand,
-        ap_uint<SIZE_N> multiplier,
-
-        ap_uint<SIZE_N> modulus
-    ){
-        ap_uint<SIZE_N> result;
-        ap_uint<SIZE_N> temp_1;
-        ap_uint<SIZE_N> temp_2;
-
-        result = 0;
-
-        for(int i = 0; i < SIZE_N; i++){
-
-            temp_1 = multiplicand[i] ? multiplier : ap_uint<SIZE_N>(0);
-            temp_2 = result + temp_1;
-
-            if(temp_2[0] == 0){ //even
-                result = temp_2 >> 1;   //divide by 2
-            } else{
-                result = (temp_2 + modulus) >> 1; //divide by 2
-            }
-        }
-
-        return result;
-    }
-
-#elif(MONT_IMP == 2)
-    ap_uint<SIZE_N> rad2_mont_mult(
-        ap_uint<SIZE_N> multiplicand,
-        ap_uint<SIZE_N> multiplier,
-
-        ap_uint<SIZE_N> modulus
-    ){
-        ap_uint<SIZE_N> result;
-        ap_uint<1> temp_1;
-
-        ap_uint<SIZE_N> temp_2;
-        ap_uint<SIZE_N> temp_3;
-
-        result = 0;
-
-        for(int i = 0; i < SIZE_N; i++){
-            temp_1 = result[0] ^ (multiplicand[i] & multiplier[0]);
-            temp_2 = temp_1? modulus : ap_uint<SIZE_N>(0);
-            temp_3 = multiplicand[i] ? multiplier : ap_uint<SIZE_N>(0);
-
-            result = (result + temp_3 + temp_2) >> 1;
-        }
-
-        return result;
-    }
-#endif
-
-ap_uint<SIZE_N> mont_mod_mult(
-
-#ifndef CONST_N
-	ap_uint<SIZE_N> R_SQUARED,
-#endif
-
-	ap_uint<SIZE_N> multiplicand,
-	ap_uint<SIZE_N> multiplier,
-	ap_uint<SIZE_N> modulus
-){
-	ap_uint<SIZE_N> temp_1;
-	ap_uint<SIZE_N> result;
-
-	temp_1 = rad2_mont_mult(multiplicand, multiplier, modulus);
-	result = rad2_mont_mult(temp_1, R_SQUARED, modulus);
-
-    return result;
-}
-
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-
-//(a*b mod n)
-ap_uint<SIZE_N> mod_mult(
-	ap_uint<SIZE_N> a,
-	ap_uint<SIZE_N> b,
-	ap_uint<SIZE_N> modulus
-){
-	ap_uint<SIZE_N*2> temp;
-	ap_uint<SIZE_N*2> quotient;
-	ap_uint<SIZE_N*2> remainder;
-	ap_uint<SIZE_N> result;
-
-	temp     = (a * b);
-	quotient = temp / modulus;
-	remainder = temp - quotient * modulus;
-
-	return result = remainder(SIZE_N-1,0);
-}
-
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-
-/*	SQUARE-AND-MULTPLY EXPONENTIATION	*/
-
-ap_uint<SIZE_N> sqrmul_exp(
-#ifndef CONST_N
-	ap_uint<SIZE_N> R_SQUARED,
-#endif
-	ap_uint<SIZE_N> base,
-	ap_uint<SIZE_N> exponent,
-
-	ap_uint<SIZE_N> modulus
-){
-	ap_uint<SIZE_N> result;
-	ap_uint<SIZE_N> temp;
-
-	if(exponent == 0) return ap_uint<SIZE_N>(1);
-
-	temp = base;
-
-	if(exponent[0] == 1)result = base;
-	else result = 1;
-
-	for(int i = 0; i < SIZE_N; i++){
-		temp = mont_mod_mult(R_SQUARED, temp, temp, modulus);
-		exponent = exponent >> 1;
-		if(exponent & 1) result = mont_mod_mult(R_SQUARED, result, temp, modulus);
-	}
-
-	return result;
-}
-
-/*	MONTGOMREY EXPONENTIATION	*/
-ap_uint<SIZE_N> mont_exp(
-	ap_uint<SIZE_N> base_prime,
-	ap_uint<SIZE_N> exponent,
-	ap_uint<SIZE_N> i_prime,
-	ap_uint<SIZE_N> modulus
-){
-
-	ap_uint<SIZE_N> z;
-	ap_uint<SIZE_N> result;
-
-	z = base_prime;
-	result = i_prime;
-
-	for(int i = 0; i < SIZE_N; i++){
-		if(exponent[i] == 1)
-			result = rad2_mont_mult(result, z, modulus);
-
-		z = rad2_mont_mult(z, z, modulus);
-	}
-
-	result = rad2_mont_mult(result, z, modulus);
-	result = rad2_mont_mult(result, 1, modulus);
-
-	return result;
-}
-
 ap_uint<SIZE_N> paillier_encrypt(
 	ap_uint<SIZE_N> i_prime,
 	ap_uint<SIZE_N> square_r,
@@ -217,4 +29,168 @@ ap_uint<SIZE_N> paillier_encrypt(
 
 	result = mont_mod_mult(square_r, temp_1, temp_2, n_squared);
 	return result;
+}
+
+ap_uint<SIZE_N> paillier_decrypt(
+	ap_uint<SIZE_N> i_prime,
+	ap_uint<SIZE_N> square_r,
+
+	ap_uint<SIZE_N> cipher,
+
+	ap_uint<SIZE_N> key_lambda,
+	ap_uint<SIZE_N> key_mu,
+
+	ap_uint<SIZE_N> n,
+	ap_uint<SIZE_N> n_squared
+){
+	ap_uint<SIZE_N> cipher_prime;
+	ap_uint<SIZE_N> temp_1;
+
+	ap_uint<SIZE_N> l_result;
+
+	///c^lambda mod n^2
+	cipher_prime = mont_mod_mult(square_r, cipher, R_CONST, n_squared);
+	temp_1 = mont_exp(cipher_prime, key_lambda, i_prime, n_squared);
+
+	//L = (x-1) / n
+	l_result = (temp_1 - 1) / n;
+
+	return mont_mod_mult(square_r, l_result, key_mu, n);
+}
+
+void  paillier_key_gen(
+		ap_uint<SIZE_N> i_prime,
+		ap_uint<SIZE_N> square_r,
+
+		ap_uint<SIZE_N/2> p,
+		ap_uint<SIZE_N/2> q,
+
+		ap_uint<SIZE_N> *n,
+		ap_uint<SIZE_N> *g,
+
+		ap_uint<SIZE_N> *lambda,
+		ap_uint<SIZE_N> *mu
+){
+
+	ap_uint<SIZE_N> n_temp;
+	ap_uint<SIZE_N> g_temp;
+
+	ap_uint<SIZE_N> lambda_temp;
+	ap_uint<SIZE_N> mu_temp;
+
+	n_temp = p*q;
+
+	ap_uint<SIZE_N> n_squared;
+	n_squared = n_temp * n_temp;
+
+	lambda_temp = ((p-1)/gcd(p-1, q-1))*(q-1);
+
+
+	for(int i = 0; i < (n_squared) - 1; i++){
+		if(gcd(n_squared, i) == 1) g_temp = i;	//TODO: g should be randomly chosen from a set that meets the conditions
+	}
+
+
+	ap_uint<SIZE_N> g_prime;
+	g_prime = mont_mod_mult(square_r, g_temp, R_CONST, n_squared);
+
+	ap_uint<SIZE_N> temp_1;
+	temp_1 = mont_exp(g_prime, lambda_temp, i_prime, n_squared);
+
+	ap_uint<SIZE_N> temp_2;
+	temp_2 = (temp_1 - 1) / n_temp;
+	mu_temp = modInverse(temp_2, n_temp);
+
+	*n      = n_temp;
+	*g      = g_temp;
+	*lambda = lambda_temp;
+	*mu     = mu_temp;
+}
+
+void paillier(
+	//enc = 1, dec = 2, noop = 0
+	ap_uint<2> operation,
+
+	ap_uint<SIZE_N*2> data_in,
+
+	ap_uint<SIZE_N/2> p,
+	ap_uint<SIZE_N/2> q,
+
+	ap_uint<SIZE_N*2> *data_out
+){
+
+	ap_uint<SIZE_N> n;
+	ap_uint<SIZE_N*2> n_squared;
+	ap_uint<SIZE_N> lambda;
+	ap_uint<SIZE_N> index;
+	ap_uint<SIZE_N> g;
+	ap_uint<SIZE_N> g_prime;
+	ap_uint<SIZE_N> temp_1;
+	ap_uint<SIZE_N> temp_2;
+	ap_uint<SIZE_N> mu;
+
+	ap_uint<SIZE_N> R_REDUCED;
+	ap_uint<SIZE_N> R_SQUARED;
+
+	ap_uint<SIZE_N*2> R2_REDUCED;
+	ap_uint<SIZE_N*2> R2_SQUARED;
+
+	/********************************************/
+
+	ap_uint<SIZE_N*2> temp_3;
+
+	ap_uint<SIZE_N*2> temp_4;
+	ap_uint<SIZE_N> r_prime;
+
+	ap_uint<SIZE_N> result;
+
+	ap_uint<SIZE_N> cipher_prime;
+
+	ap_uint<SIZE_N> l_result;
+
+	if(operation == 3){	//if enc or dec
+		//key Generation
+		n = p * q;
+		R_REDUCED = barrett_reduction(R_CONST ,n); //i_prime
+		R_SQUARED = barrett_reduction(R_CONST ,n); //
+
+		n_squared = n * n;
+		R2_REDUCED = barrett_reduction2(R_CONST2, n_squared);
+		R2_SQUARED = barrett_reduction2(R_CONST2, n_squared);
+
+		lambda = ((p-1)/gcd(p-1, q-1))*(q-1);
+
+		index =  pseudo_random(n, 1);
+
+		for(int i = 0; i < n; i++){
+			if(gcd(n_squared, i) == 1){
+				if(i >= index) g = i;
+			}
+		}
+
+		g_prime = mont_mod_mult(R2_SQUARED, g, R_CONST, n_squared); /*||||||||*/
+
+		temp_1 = mont_exp(g_prime, lambda, R2_REDUCED, n_squared);
+
+		temp_2 = (temp_1 - 1) / n;
+		mu = modInverse(temp_2, n);
+
+	}else if(operation == 1){
+		temp_3 = mont_exp(g_prime, data_in, R2_REDUCED, n_squared);
+
+		r_prime = pseudo_random(0, 0);
+		temp_4 = mont_exp(r_prime, n, R2_REDUCED, n_squared);
+
+		*data_out = mont_mod_mult(R2_SQUARED, temp_3, temp_2, n_squared);
+
+	} else if(operation == 2){
+		///c^lambda mod n^2
+		cipher_prime = mont_mod_mult(R2_SQUARED, data_in, R_CONST, n_squared);
+		temp_3 = mont_exp(cipher_prime, lambda, R2_REDUCED, n_squared);
+
+		//L = (x-1) / n
+		l_result = (temp_1 - 1) / n;
+
+		*data_out = mont_mod_mult(R_SQUARED, l_result, mu, n);
+	}
 }
